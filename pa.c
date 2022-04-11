c04318800a555aa1183dc733c8b9a267b030681b
ferhadme <frhdmehdiyev@gmail.com>
2022-04-12 01:33:18 +0400

    showing balance broker

--- pa.c
+++ pa.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 #include "pstack.h"
+
+#define RED_CLR "\x1B[31m"
+#define RESET_CLR "\x1B[0m"
+
 
 bool is_left_paranthese(char p)
 {
@@ -12,14 +18,25 @@ bool is_right_paranthese(char p)
     return p == ')' || p == ']' || p == '}';
 }
 
-/* Assuming arg is only right paranthese */
 char flip(char p)
 {
+    assert(is_right_paranthese(p));
     if (p == ')')
 	return '(';
     if (p == '}')
 	return '{';
     return '[';
+}
+
+void unbalanced(char *p_exp, p_stack *ps, int margin)
+{
+    printf("Unbalanced parantheses\n");
+    printf("%s\n", p_exp);
+    if (is_right_paranthese(*(p_exp + margin)))
+	margin++;
+    printf("%s%*s%s\n", RED_CLR, margin, "^", RESET_CLR);
+    free_p_stack(ps);
+    exit(1);
 }
 
 int main(int argc, char **argv) {
@@ -30,32 +47,24 @@ int main(int argc, char **argv) {
     char *p_exp = *(argv + 1);
 
     p_stack *ps = create_p_stack();
+    int margin = 0;
 
     for (char *p = p_exp; *p != '\0'; p++) {
 	if (is_left_paranthese(*p)) {
 	    push(ps, *p);
 	} else if (is_right_paranthese(*p)) {
 	    if (pop(ps) != flip(*p)) {
-		printf("Unbalanced parantheses\n");
-		free_p_stack(ps);
-		return 1;
+		unbalanced(p_exp, ps, margin);
 	    }
 	}
+	margin++;
     }
 
     if (!is_empty(ps)) {
-	printf("Unbalanced parantheses\n");
-	free_p_stack(ps);
-	return 2;
+	unbalanced(p_exp, ps, margin);
     }
 
     printf("Balanced parantheses\n");
     free_p_stack(ps);
     return 0;
 }
-
-/*
-TODO: count paranthes pairs
-TODO: count paranthes individually
-TODO: Show where balance broke
-*/
